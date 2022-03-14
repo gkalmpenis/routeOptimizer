021f61946d50540e696f6659a8c25cc58f297988
GiannisGkalmpenis <g.gkalmpenis@gmail.com>
2022-03-14 23:24:34 +0200

    created bottom sheet view model class

---
+++ app/src/main/java/com/example/routeoptimizer/viewmodels/BottomSheetViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.routeoptimizer.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BottomSheetViewModel: ViewModel() {
+    // Create a LiveData with a String
+    val currentName: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    // Rest of the ViewModel...
+}
